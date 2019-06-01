9a4db7cddbad0966d7177bdfe01b04ae89d998ee
Sho Ishikura <shoishikura@gmail.com>
2019-06-01 20:07:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ return [element, ...array]
 }
 
 function destructivelyaddElementToBeginningOfArray(array, element) {
-return array.unshift("element")
+return element.unshift("array")
 }
 
 function addElementToEndOfArray(array, element)  {
